ddc26f2109cda9019441ec0e30ca8d5b2a5c9e78
yylaji <yylaji@gmail.com>
2021-03-02 12:12:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+sed -i '$a src-git hongwenjun https://github.com/hongwenjun/openwrt-luci-kcp-udp' feeds.conf.default
